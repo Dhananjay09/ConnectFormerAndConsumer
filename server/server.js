0a7384618c89cc4c898b475a2be36dcec6c9c17a
Dhananjay09 <dj091298@gmail.com>
2020-08-28 09:30:39 +0530

    Done Succefully last commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true,
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
-
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static('build'))
+  const path = require('path')
+  app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
